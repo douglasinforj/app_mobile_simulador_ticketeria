2eda41c8645ecf73aeed7acf4b5c1a2f727c692d
Douglas Silva <douglasitpro@gmail.com>
2025-02-05 10:28:32 -0300

    Ajustes

--- components/navbar.py
+++ components/navbar.py
@@ -1,8 +1,11 @@
-import flet as ft 
+import flet as ft
 
 def navbar(page):
-    return ft.Row([
-        ft.ElevatedButton("Início", on_click=lambda _: page.go("/")),
-        ft.ElevatedButton("Comprar", on_click=lambda _: page.go("/comprar")),
-        ft.ElevatedButton("Validar", on_click=lambda _: page.go("/validar")),
-    ])
+    return ft.Row(
+        controls=[
+            ft.ElevatedButton("Início", on_click=lambda _: page.go("/")),
+            ft.ElevatedButton("Comprar", on_click=lambda _: page.go("/comprar")),
+            ft.ElevatedButton("Validar", on_click=lambda _: page.go("/validar")),
+        ],
+        alignment=ft.MainAxisAlignment.CENTER
+    )
