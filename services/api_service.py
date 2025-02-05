d98338e499f595a203b7f5303651cd49cfc25ba5
Douglas Silva <douglasitpro@gmail.com>
2025-02-05 12:22:18 -0300

    Adicionando função para buscar Evento da api

--- services/api_service.py
+++ services/api_service.py
@@ -2,9 +2,9 @@ import requests
 
 API_URL = "http://localhost:8000/api/"
 
-def comprar_ingresso(nome, cpf):
+def comprar_ingresso(nome, cpf, evento_id):
     try:
-        response = requests.post(f"{API_URL}ingressos/", json={"nome": nome, "cpf":cpf})
+        response = requests.post(f"{API_URL}ingressos/", json={"nome": nome, "cpf": cpf, "evento_id": evento_id})
         if response.status_code == 201:
             ingresso = response.json()
             return {"status": True, "mensagem": f"Ingresso Comprado! Código:{ingresso['codigo_ingresso']}"}
@@ -17,6 +17,16 @@ def validar_ingresso(codigo):
         response = requests.get(f"{API_URL}validar-ingresso/{codigo}/")
         if response.status_code == 200:
             return {"status": True, "mensagem": "Ingresso válido!"}
-        return {"status": False, "mensagem":"Ingresso inválido!"}
+        return {"status": False, "mensagem": "Ingresso inválido!"}
+    except Exception as e:
+        return {"status": False, "mensagem": str(e)}
+
+# Função para buscar eventos da API
+def buscar_eventos():
+    try:
+        response = requests.get(f"{API_URL}eventos/")
+        if response.status_code == 200:
+            return response.json()  # Retorna a lista de eventos
+        return []
     except Exception as e:
         return {"status": False, "mensagem": str(e)}
